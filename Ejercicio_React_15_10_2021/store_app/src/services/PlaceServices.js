929562052d09398371f5d1f6f8e97d73fd77a6c6
Daniel <richerdani3@gmail.com>
2021-12-12 14:06:57 -0500

    Implementando backend

---
+++ Ejercicio_React_15_10_2021/store_app/src/services/PlaceServices.js
@@ -0,0 +1,26 @@
+import http from './http-config';
+
+const getAll = () =>{
+    return http.get("/places/");
+}
+const get = (id)=>{
+    return http.get(`/places/${id}`,id);
+}
+const create = (placeData)=>{
+    return http.post("/places",placeData)
+}
+const update =(id,placeData)=>{
+    return http.put(`/places/${id}`,placeData)
+}
+const remove = id =>{
+    return http.delete(`/places/${id}`);
+}
+
+const exportedObjects = {
+    getAll,
+    get,
+    create,
+    update,
+    remove
+}
+export default exportedObjects;
